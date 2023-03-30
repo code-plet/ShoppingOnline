4f1a136d0e2fffa212994a46c4bfbf196135c31c
code-plet <70930547+code-plet@users.noreply.github.com>
2023-03-30 16:40:45 +0700

    LoginTextboxes
    -Username textbox reactive focus.
    -Password textbox reactive focus.

--- ShoppingOnline/Form1.Designer.cs
+++ ShoppingOnline/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShoppingOnline
 {
-    partial class Form1
+    partial class ShoppingOnline
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ShoppingOnline.Properties.Resources.Big_Shopping_Mall_Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.usernameTextbox.Location = new System.Drawing.Point(159, 25);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(330, 22);
+            this.usernameTextbox.TabIndex = 1;
+            this.usernameTextbox.Text = "Nhập tên tài khoản";
+            this.usernameTextbox.Enter += new System.EventHandler(this.usernameTextbox_Enter);
+            this.usernameTextbox.Leave += new System.EventHandler(this.usernameTextbox_Leave);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.passwordTextBox.Location = new System.Drawing.Point(159, 65);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(305, 22);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Text = "Nhập mật khẩu";
+            this.passwordTextBox.Enter += new System.EventHandler(this.passwordTextBox_Enter);
+            this.passwordTextBox.Leave += new System.EventHandler(this.passwordTextBox_Leave);
+            // 
+            // ShoppingOnline
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(501, 178);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "ShoppingOnline";
+            this.Text = "ShoppingOnline";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox passwordTextBox;
     }
 }
 
--- ShoppingOnline/Form1.cs
+++ ShoppingOnline/Form1.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace ShoppingOnline
 {
-    public partial class Form1 : Form
+    public partial class ShoppingOnline : Form
     {
-        public Form1()
+        public ShoppingOnline()
         {
             InitializeComponent();
+        }
+
+        private void usernameTextbox_Leave(object sender, EventArgs e)
+        {
+            if(usernameTextbox.Text.Length == 0)
+            {
+                usernameTextbox.Text = "Nhập tên tài khoản";
+                usernameTextbox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void usernameTextbox_Enter(object sender, EventArgs e)
+        {
+            if(usernameTextbox.Text == "Nhập tên tài khoản")
+                usernameTextbox.Clear();
+
+            usernameTextbox.ForeColor = Color.Black;
+
+        }
+
+        private void passwordTextBox_Leave(object sender, EventArgs e)
+        {
+            if (passwordTextBox.Text.Length == 0)
+            {
+                passwordTextBox.Text = "Nhập mật khẩu";
+                passwordTextBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void passwordTextBox_Enter(object sender, EventArgs e)
+        {
+            if (passwordTextBox.Text == "Nhập mật khẩu")
+                passwordTextBox.Clear();
+
+            passwordTextBox.ForeColor = Color.Black;
         }
     }
 }
